8bc2a8f521bf5e9a3b63c938c4755fa0ae6b863f
Thijs Dreef <thijs9907@gmail.com>
2019-12-08 23:30:13 +0100

    added implementation for material library

--- Source/Scarlet-Renderer/Headers/Scarlet/Materials/MaterialLibrary.h
+++ Source/Scarlet-Renderer/Headers/Scarlet/Materials/MaterialLibrary.h
@@ -1,12 +1,16 @@
 #include "Scarlet/Materials/Material.h"
+#include <vector>
 
 class MaterialLibrary
 {
 private:
     std::vector<Material*> m_Materials;
+    std::vector<unsigned int> m_FreeList;
+    unsigned int getNewId();
 public:
-    unsigned int addMaterial(Material* material);
+    unsigned int addMaterial(Material* u_Material);
     Material* getMaterial(unsigned int id);
+    void deleteMaterial(unsigned int id);
     MaterialLibrary();
     ~MaterialLibrary();
 };
---
+++ Source/Scarlet-Renderer/Source/Scarlet/Materials/MaterialLibrary.cpp
@@ -0,0 +1,38 @@
+#include "Scarlet/Materials/MaterialLibrary.h"
+
+MaterialLibrary::MaterialLibrary() {
+
+}
+
+MaterialLibrary::~MaterialLibrary() {
+    for (unsigned int i = 0; i < m_Materials.size(); i++) {
+        if (m_Materials[i]) delete m_Materials[i];
+    }
+}
+
+unsigned int MaterialLibrary::getNewId() {
+    unsigned int id = m_FreeList.size();
+    id = (id > 0) ? m_FreeList[id] : m_Materials.size();
+}
+
+unsigned int MaterialLibrary::addMaterial(Material* u_Material) {
+    unsigned int id = getNewId();
+    if (id > m_Materials.size()) {
+        m_Materials.push_back(u_Material);
+    } else {
+        m_Materials[id] = u_Material;
+    }
+    return id;
+}
+
+void MaterialLibrary::deleteMaterial(unsigned int id) {
+    if (id == 0) return;
+    m_FreeList.push_back(id);
+    delete m_Materials[id];
+    m_Materials[id] = 0;
+}
+
+Material* MaterialLibrary::getMaterial(unsigned int id) {
+    Material* m = m_Materials[id];
+    return (m) ? m : m_Materials[0];
+}
